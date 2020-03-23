9bf5c5676c4d77dc5b2863f4ea7a7e7e1c024b24
jochem111 <56547780+jochem111@users.noreply.github.com>
2020-03-23 15:29:25 +0100

    border walls/sounds added
    overworld completely done
    sounds added:
    slime attack
    ghost attack
    deathsound
    getting hit
    fireing
    ambient sounds
    world music

--- Team-Valor/Assets/Jochem/Scripts/Convo/Convo.cs
+++ Team-Valor/Assets/Jochem/Scripts/Convo/Convo.cs
@@ -20,7 +20,7 @@ public class Convo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");          
     }
 
     // Update is called once per frame
