4e98ff6dc738061f047646adab5abe70d2966713
TotallyJay <TheSolidEagle@gmail.com>
2020-03-26 12:53:00 +0100

    Added assets
    Added fireball Prefab
    Added shooting script
    Added Projectile movement script

---
+++ Team-Valor/Assets/Jurrien/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Vector3 dir;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Team-Valor/Assets/Jurrien/Scripts/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject projectile;
+    public Vector3 offset;
+    public Transform spawnLoc;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(projectile, spawnLoc.position, spawnLoc.rotation);
+        }
+    }
+}
